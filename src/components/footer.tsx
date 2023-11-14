9a589f6b888b0120cee2b5be723590214c0754f5
jesse1zhang@gmail.com <43320266+z-jesse@users.noreply.github.com>
2023-11-13 23:51:41 -0800

    foot up

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,21 +2,21 @@ import Social from "./social";
 
 export default function Footer() {
   return (
-    <div className="w-full md:h-44 bg-black">
-      <div className="flex flex-col md:flex-row max-w-7xl h-full mx-auto py-8 px-4 space-y-2">
+    <div className="w-full md:h-auto bg-black">
+      <div className="flex flex-col md:flex-row max-w-7xl mx-auto py-8 px-4 space-y-6 md:space-y-0">
         <div className="w-full md:w-1/3 space-y-2">
-          <p className="font-mono text-white">2023 - Crux Rewards</p>
           <Social />
+          <p className="font-mono text-white text-sm">2023 - Crux Rewards</p>
         </div>
         <div className="w-full md:w-1/3 space-y-2">
-          <p className="font-mono text-white hover:underline">Terms & Conditions</p>
-          <p className="font-mono text-white hover:underline">Privacy Policy</p> 
-          <p className="font-mono text-white hover:underline">Help & Support</p>
+          <p className="font-mono text-white text-sm hover:underline">Terms & Conditions</p>
+          <p className="font-mono text-white text-sm hover:underline">Privacy Policy</p> 
+          <p className="font-mono text-white text-sm hover:underline">Help & Support</p>
         </div>
         <div className="w-full md:w-1/3 space-y-2">
-          <p className="font-mono text-white hover:underline">Contact Us</p>
-          <p className="font-mono text-white hover:underline">About</p> 
-          <p className="font-mono text-white hover:underline">Careers</p>
+          <p className="font-mono text-white text-sm hover:underline">Contact Us</p>
+          <p className="font-mono text-white text-sm hover:underline">About</p> 
+          <p className="font-mono text-white text-sm hover:underline">Careers</p>
         </div>
       </div>
     </div>
